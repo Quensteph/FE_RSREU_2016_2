af8d7dbccd9f077a3dfa483d182a05357d25114a
Quensteph <yuramonogatari@gmail.com>
2016-11-10 10:40:40 +0300

    objects fix

--- objects/js/objects.js
+++ objects/js/objects.js
@@ -5,12 +5,18 @@ var testObject = {
   "secondName": "Example",
   "age": 999,
   "isHuman": true,
+  "luggage": {
+    "item1": 1,
+    "item2": 2,
+    "item3": 3,
+  }
 }
 
 /* Task 1 */
 /* Write a JavaScript function to get the length of a JavaScript object. */
 
 function getObjectLength(obj) {
+  var length = Object.keys(obj).length;
   return Object.keys(obj).length;
 }
 
@@ -20,13 +26,22 @@ console.log(getObjectLength(testObject));
 /* Task 2 */
 /* Write a JavaScript function to list the properties of a JavaScript object. */
 
-function getObjectProperties(obj) {
-  var keys = [];
-  for (var key in obj) {
-    keys.push(key);
-  }
 
-  return keys;
+
+function getObjectProperties(obj) {
+  var result = 0;
+
+  return (function _getObjectProperties(obj) {
+    for (var key in obj) {
+      if (typeof obj[key] === "object") {
+        _getObjectProperties(obj[key]);
+      } else {
+        result++;
+      }
+    }
+
+    return result;
+  } (obj));
 }
 
 console.log("getObjectProperties");
